b145f21f7b88b4b2054dd1c1194da43133784343
Ermiyas Dagnachew <ermiyas.dagnachew@gmail.com>
2024-02-11 20:50:16 +0300

    Done with the starter

---
+++ components/ScrollOffers.tsx
@@ -0,0 +1,21 @@
+'use client'
+import { IoIosArrowRoundDown } from 'react-icons/io'
+
+const ScrollOffers = () => {
+    const handleScroll = () => {
+        const offsetTop = document.getElementById('offers')?.offsetTop
+        if(offsetTop){
+            window.scrollTo({
+                top: offsetTop + 60,
+                behavior: 'smooth'
+            })
+        }
+      }
+  return (
+    <div className="absolute w-24 h-24 translate-x-[-50%] translate-y-[calc(100vh-70px)] bg-[rgba(47,64,75,1)] top-0 left-[35%] z-[10000] flex items-center justify-center cursor-pointer" onClick={handleScroll}>
+          <IoIosArrowRoundDown className="text-[#BEC2C5] font-thin w-12 h-12" />
+        </div>
+  )
+}
+
+export default ScrollOffers
