270267b08c6e8412655da2d6e9b98c350529adf3
Fasika Dagnachew <fasika1515@gmail.com>
2024-02-23 12:19:15 +0300

    pricing web done

--- app/pricing/WebPricing.tsx
+++ app/pricing/WebPricing.tsx
@@ -14,10 +14,10 @@ export default function WebPricing() {
             <div className='absolute right-0 bottom-[160px] translate-x-[300px] translate-y-1/2 w-[1000px] h-[1000px] bg-[radial-gradient(#43D0F755,_transparent)] blur-[100px] rounded-full'></div>
             <h1 className='text-4xl font-black'><span className='text-[#43D0F7]'>Web</span> Pricing</h1>
             <div className="w-full flex justify-center items-center py-10 gap-5">
-                <div className={`px-8 py-2 text-2xl font-semibold rounded-full cursor-pointer duration-300 border-2 border-[#ffffff00] hover:border-[#fff] ${wType === "static" && 'border-[#43D0F7ff] shadow-[0px_5px_10px_0px_#43D0F755]'}`} onClick={() => dispatch(changeWType("static"))}>Static Web Presence</div>
-                <div className={`px-8 py-2 text-2xl font-semibold rounded-full cursor-pointer duration-300 border-2 border-[#ffffff00] hover:border-[#fff] ${wType === "dynamic" && 'border-[#43D0F7ff] shadow-[0px_5px_10px_0px_#43D0F755]'}`} onClick={() => dispatch(changeWType("dynamic"))}>Dynamic Web Application</div>
+                <div className={`px-8 py-2 text-2xl font-semibold rounded-full cursor-pointer duration-300 border-2 border-[#ffffff00] ${wType === "static" && 'border-[#43D0F7ff_!important] shadow-[0px_5px_10px_0px_#43D0F755]'}`} onClick={() => dispatch(changeWType("static"))}>Static Web Presence</div>
+                <div className={`px-8 py-2 text-2xl font-semibold rounded-full cursor-pointer duration-300 border-2 border-[#ffffff00] ${wType === "dynamic" && 'border-[#43D0F7ff_!important] shadow-[0px_5px_10px_0px_#43D0F755]'}`} onClick={() => dispatch(changeWType("dynamic"))}>Dynamic Web Application</div>
             </div>
-            <p className='w-full max-w-[800px] text-2xl text-center mb-16'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat dolores id, autem rem ea rerum deleniti aut accusantium ad dicta?</p>
+            <p className='w-full max-w-[800px] text-lg text-center mb-16'>Unlock the full potential of your business with our affordable pricing plans. Choose the perfect fit and let's grow together!</p>
             <WebCards />
         </div>
     )
