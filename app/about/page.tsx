694450758e2a5e56423f1ded1d886d5ef805aff4
Fasika Dagnachew <fasika1515@gmail.com>
2024-03-07 03:01:02 +0300

    about us page started

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import AboutUs from './AboutUs'
+import Button from '../../components/Button'
+import OurMission from './OurMission'
 
-const page = () => {
+export default function page() {
   return (
-    <div>page</div>
+    <div className='w-full flex flex-col items-center'>
+      <AboutUs />
+      <Button right={false} href={"/services"} name={"Explore"} />
+      <OurMission />
+    </div>
   )
 }
-
-export default page
